17ef76c0f5fe41859b71d7b1ca26d018f52779f5
julianmackenzie <jbm5@uic.edu>
2024-03-10 20:21:05 -0500

    Healthbars added
    CharacterData class added with Health variable, attached to empty Newborn and Death objects.

---
+++ Death Lives, Life Dies/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+
+    public GameObject characterToWatch;  // "character" to watch (death or newborn)
+    public int barVal;  // value displayed on healthbar
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterToWatch.GetComponent<CharacterInfo>().health == barVal) {
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
