7badd82b089648db0f183c2b97fd3db7ee9e796b
FarahKamleh <fkamleh8800@gmail.com>
2024-03-13 02:45:09 -0500

    Floating Item
    The special item is now slowly rising and falling in the air.

--- Death Lives, Life Dies/Assets/Scripts/Float.cs
+++ Death Lives, Life Dies/Assets/Scripts/Float.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class Float : MonoBehaviour
 {
     // speed of the floating
-    float speed = 1.0f;
+    float speed = 1.5f;
 
     // how high and low the object floats
     float height = 0.2f;
 
+    // vector for original position
+    Vector3 pos;
+
+    private void Start()
+    {
+        // maintain/begin with original position
+        pos = transform.position;
+    }
+
     void Update()
     {
-        // maintain original position
-        Vector3 pos = transform.position;
-
-        // calculate y position
-        float newY = Mathf.Sin(Time.time * speed);
+        // calculate new y position
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
 
         // set new position based on new y
-        transform.position = new Vector3(pos.x, newY * height, pos.z);
+        transform.position = new Vector3(pos.x, newY, pos.z);
     }
 }
