0a4e07cb33bdd6a7ad481014cf3de97c6abc7f26
FarahKamleh <fkamleh8800@gmail.com>
2024-03-13 00:35:13 -0500

    Add to Item Holder
    When the user collides with the special item, it is destroyed. The sprite representation then appears in the item holder to the left of the health bars.

---
+++ Death Lives, Life Dies/Assets/Scripts/Float.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+    // speed of the floating
+    float speed = 1.0f;
+
+    // how high and low the object floats
+    float height = 0.2f;
+
+    void Update()
+    {
+        // maintain original position
+        Vector3 pos = transform.position;
+
+        // calculate y position
+        float newY = Mathf.Sin(Time.time * speed);
+
+        // set new position based on new y
+        transform.position = new Vector3(pos.x, newY * height, pos.z);
+    }
+}
---
+++ Death Lives, Life Dies/Assets/Scripts/SpecialItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialItem : MonoBehaviour
+{
+    // to switch between special items in the corner
+    public SpriteRenderer empty;
+    public SpriteRenderer wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // enter if a collision occurs with the CAVE2
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if the collision is with wall special item
+        if (collision.gameObject.tag == "WallItem")
+        {
+            // destroy the special item
+            Destroy(collision.gameObject);
+
+            // swap the special item image to the tombstone
+            empty.enabled = false;
+            wall.enabled = true;
+
+        }
+    }
+}
