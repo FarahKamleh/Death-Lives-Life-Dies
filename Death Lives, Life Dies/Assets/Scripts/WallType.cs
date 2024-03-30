fcae15131e32cb346c6b6d96045fc9ba06868b03
julianmackenzie <jbm5@uic.edu>
2024-03-30 13:41:54 -0500

    Outer Wall and WallType Script Added
    Added full outer wall of the map and a script for setting the walls' models using a public boolean. Walls will look like Life in the scene view and default to Death on runtime unless the boolean is true.

---
+++ Death Lives, Life Dies/Assets/Scripts/WallType.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallType : MonoBehaviour
+{
+
+    public bool isLife = false;
+    public GameObject lifeWall;
+    public GameObject deathWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLife) {
+            lifeWall.SetActive(true);
+            deathWall.SetActive(false);
+        } else {
+            lifeWall.SetActive(false);
+            deathWall.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
