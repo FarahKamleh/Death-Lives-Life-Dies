8e493dba8193dab394c78e0867236f32e2335450
FarahKamleh <fkamleh8800@gmail.com>
2024-04-03 02:40:27 -0500

    Assistant Raises Wall
    Only when triggered by companion, the assistant raises wall when x position is that of target. However, few bugs regarding pattern with wall raise and using wall special item.

--- Death Lives, Life Dies/Assets/Scripts/AssistantBehavior.cs
+++ Death Lives, Life Dies/Assets/Scripts/AssistantBehavior.cs
@@ -18,6 +18,9 @@ public class AssistantBehavior : MonoBehaviour
     private bool followTarget;
     private bool followOther;
 
+    // wall to be raised
+    public GameObject wall;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +37,14 @@ public class AssistantBehavior : MonoBehaviour
         if (followOther)
         {
             agent.SetDestination(otherTarget.position);
+
+            // use x position to raise wall and remove assistant
+            if (gameObject.transform.position.x == otherTarget.position.x)
+            {
+                // raise wall of opposing player and make assistant disappear
+                wall.SetActive(true);
+                StartCoroutine(MoveFunctionUp());
+            }
         }
     }
 
@@ -54,6 +65,35 @@ public class AssistantBehavior : MonoBehaviour
     private IEnumerator WalkUntilDeath() {  // kill after interval
         yield return new WaitForSeconds(deathTime);
         gameObject.SetActive(false);
+    }
+
+    // move the wall
+    IEnumerator MoveFunctionUp()
+    {
+        // detatch trees from parent
+        wall.transform.parent = null;
+        float totalTime = 3;
+        float elapsedTime = 0;
+
+        while (true)
+        {
+            // gradually make the wall rise at the speed of time
+            elapsedTime += Time.deltaTime;
+            wall.transform.position = Vector3.Lerp(wall.transform.position, new Vector3(wall.transform.position.x, 1, wall.transform.position.z), elapsedTime / totalTime);
+
+            // if the wall reached the desired height, exit
+            if (wall.transform.position == new Vector3(wall.transform.position.x, 1, wall.transform.position.z))
+            {
+                // exit
+                wall.GetComponent<TreeWallController>().activated = true;
+
+                // make assistant disappear
+                gameObject.SetActive(false);
+                yield break;
+            }
+
+            yield return null;
+        }
     }
 
 }
