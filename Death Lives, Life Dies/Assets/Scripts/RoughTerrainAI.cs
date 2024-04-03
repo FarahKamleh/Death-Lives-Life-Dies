12f1ebc6b65521fc605bd266398a19b38c34c647
julianmackenzie <jbm5@uic.edu>
2024-04-02 22:22:19 -0500

    Rough Terrain AI added
    Rough Terrain chooses a random player to navigate to, and a random interval, after which it switches targets to the other player and repeats.

---
+++ Death Lives, Life Dies/Assets/Scripts/RoughTerrainAI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class RoughTerrainAI : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform target;
+
+    [SerializeField] private Transform lifePosition;
+    [SerializeField] private Transform deathPosition;
+
+    [SerializeField] private bool doSwitch;
+    [SerializeField] private float switchInterval;
+
+    private void Start() {
+        Random.seed = System.DateTime.Now.Millisecond;  // randomness seed
+        if (Random.Range(0.0f, 1.0f) < 0.5f) {  // choose starting target
+            target = deathPosition;
+        } else {
+            target = lifePosition;
+        }
+        doSwitch = true;  // enable switch coroutine
+    }
+    private void Update() {  // set destination to target player, start switch coroutine
+        if (target != null) {
+            agent.SetDestination(target.position);
+        }
+
+        if (doSwitch == true) {
+            doSwitch = false;  // disable coroutine until finished
+            StartCoroutine(PlayerSwitch());
+        }
+    }
+
+    private IEnumerator PlayerSwitch() {  // choose a random switch interval and switch target after
+        switchInterval = Random.Range(20.0f, 45.0f);
+        yield return new WaitForSeconds(switchInterval);
+        if (target == deathPosition) {
+            target = lifePosition;
+        } else {
+            target = deathPosition;
+        }
+        doSwitch = true;  // re-enable switch coroutine
+    }
+
+}
