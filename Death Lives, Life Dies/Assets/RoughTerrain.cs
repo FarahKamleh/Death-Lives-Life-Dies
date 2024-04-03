8d39ceab261b22631780fb4e8d9b7ebe325a83d8
julianmackenzie <jbm5@uic.edu>
2024-04-02 20:54:26 -0500

    Rough Terrain Effect Added
    Slows players by 25% if touching the rough terrain (a circular patch of fog)

---
+++ Death Lives, Life Dies/Assets/RoughTerrain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoughTerrain : MonoBehaviour
+{
+
+    public float slowMoveSpeed;
+    private float normalMoveSpeed;
+
+    void Start()
+    {
+        normalMoveSpeed = gameObject.GetComponent<CAVE2WandNavigator>().movementScale;
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("RoughTerrain")) {
+            gameObject.GetComponent<CAVE2WandNavigator>().movementScale = slowMoveSpeed;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("RoughTerrain")) {
+            gameObject.GetComponent<CAVE2WandNavigator>().movementScale = normalMoveSpeed;
+        }
+    }
+
+}
