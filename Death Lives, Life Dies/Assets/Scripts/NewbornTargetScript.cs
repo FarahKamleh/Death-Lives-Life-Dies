6126be8d1669abf7da31e5d41ef251a265b34284
maiuryn <sebastianho0311@gmail.com>
2024-04-09 19:41:58 -0500

    Update AI

--- Death Lives, Life Dies/Assets/Scripts/NewbornTargetScript.cs
+++ Death Lives, Life Dies/Assets/Scripts/NewbornTargetScript.cs
@@ -11,7 +11,14 @@ public class NewbornTargetScript : MonoBehaviour
     [SerializeField] Transform newbornTransform;
 
     // Landmarks
+    Transform[] landmarks;
     [SerializeField] Transform altarTransform;
+    [SerializeField] Transform flowerGardenTransform;
+    [SerializeField] Transform fountainTransform;
+    [SerializeField] Transform graveyardTransform;
+    [SerializeField] Transform pastureTransform;
+    [SerializeField] Transform pondTransform;
+    [SerializeField] Transform skullTowerTransform;
 
     float lifeDistance;
     float deathDistance;
@@ -25,6 +32,15 @@ public class NewbornTargetScript : MonoBehaviour
     void Start()
     {
         targetTransform = gameObject.GetComponent<Transform>();
+        landmarks = new[] {
+                    altarTransform,
+                    flowerGardenTransform,
+                    fountainTransform,
+                    graveyardTransform,
+                    pastureTransform,
+                    pondTransform,
+                    skullTowerTransform
+        };
     }
 
     // Update is called once per frame
@@ -36,31 +52,64 @@ public class NewbornTargetScript : MonoBehaviour
         //if ((lifeDistance < 10) && (deathDistance < 10))
         if (!cooldownStatus)
         {
-            if ((lifeDistance < 0) && (deathDistance < 0))
+            if ((lifeDistance < 5) && (deathDistance < 5))
             {
-                // Head towards a random landmark, but Altar, for now
-                targetTransform.position = altarTransform.position;
+                // Head towards a random landmark
+                Vector3 center = average(lifeTransform.position, deathTransform.position);
+                targetTransform.position = getFurthestLandmarkTransform(center);
+                cooldownStatus = true;
+                StartCoroutine(cooldown());
             }
             else if (lifeDistance < deathDistance)
             {
                 // Run away from life
 
                 Vector3 direction = -(lifeTransform.position - newbornTransform.position).normalized;
-                direction = applyRandomAngle(direction);
-                targetTransform.position = newbornTransform.position + direction;
-
+                direction = applyRandomAngle(direction, "life");
+                targetTransform.position = direction;
+                //targetTransform.position = average(targetTransform.position, getFurthestLandmarkTransform(lifeTransform.position).position);
             }
             else
             {
                 // Run away from death
                 Vector3 direction = -(deathTransform.position - newbornTransform.position).normalized;
-                direction = applyRandomAngle(direction);
-                targetTransform.position = newbornTransform.position + direction;
+                direction = applyRandomAngle(direction, "death");
+                targetTransform.position = direction;
+                //targetTransform.position = average(targetTransform.position, getFurthestLandmarkTransform(deathTransform.position).position);
             }
         }
     }
 
-    Vector3 applyRandomAngle(Vector3 direction)
+    Vector3 average(Vector3 a, Vector3 b)
+    {
+        Vector3 center = new Vector3((a.x + b.x) / 2.0f, (a.y + b.y) / 2.0f, (a.z + b.z) / 2.0f);
+        return center;
+    }
+
+    Vector3 getFurthestLandmarkTransform(Vector3 target)
+    {
+        Transform furthest = null;
+        float furthestDistance = float.MinValue;
+
+        for (int i = 0; i < landmarks.Length; i++)
+        {
+            Transform landmark = landmarks[i];
+
+            //float currDistance = Vector3.Distance(lifeTransform.position, landmark.position) + Vector3.Distance(deathTransform.position, landmark.position);
+            float currDistance = Vector3.Distance(target, landmark.position);
+
+            if (currDistance > furthestDistance)
+            {
+                furthest = landmark;
+                furthestDistance = currDistance;
+            }
+        }
+
+        Debug.Log(furthest);
+        return furthest.position;
+    }
+
+    Vector3 applyRandomAngle(Vector3 direction, string target)
     {
         if (!cooldownStatus)
         {
@@ -74,14 +123,19 @@ public class NewbornTargetScript : MonoBehaviour
         Vector3 newDirection;
         newDirection = ((targetRotation * direction) * (dotProduct / mag)).normalized;
         newDirection *= randDist;
-        //Vector3 newPosition = newbornTransform.position + newDirection;
-        //while (newPosition.x > 66 || newPosition.x < -66 || newPosition.z < -66 || newPosition.z > 66)
-        //{
-        //    targetRotation = Quaternion.AngleAxis(Random.Range(-180.0f, 180.0f), Vector3.up);
-        //    newDirection = ((targetRotation * direction) * (dotProduct / mag)).normalized;
-        //    newDirection *= Random.Range(0.0f, 20.0f); 
-        //}
-        return newDirection;
+
+        Vector3 newPosition = newbornTransform.position + newDirection;
+        if (newPosition.x > 66 || newPosition.x < -66 || newPosition.z < -66 || newPosition.z > 66)
+        {
+            //targetRotation = Quaternion.AngleAxis(Random.Range(-180.0f, 180.0f), Vector3.up);
+            //newDirection = ((targetRotation * direction) * (dotProduct / mag)).normalized;
+            //newDirection *= Random.Range(0.0f, 20.0f); 
+            if (target == "life")
+                return getFurthestLandmarkTransform(lifeTransform.position);
+            else if (target == "death")
+                return getFurthestLandmarkTransform(deathTransform.position);
+        }
+        return newPosition;
     }
 
     IEnumerator cooldown()
