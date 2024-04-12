67da66a62ab2178fcba1eb8dd1fa0679f09a0607
FarahKamleh <fkamleh8800@gmail.com>
2024-04-12 15:12:36 -0500

    Y Axis Tracking
    A test to ensure the UI is tracked to the user's head.

---
+++ Death Lives, Life Dies/Assets/Scripts/TrackedYAxis.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackedYAxis : MonoBehaviour
+{
+    // to know which tracked head (player 1 (1) vs player 2 (10))
+    public int headID;
+
+    // empty game object to be turned
+    public GameObject emptyHead;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // fetch rotation of head tracking
+        Vector3 headRotation = CAVE2.GetHeadRotation(headID).eulerAngles;
+
+        // apply y rotation to empty game object
+        emptyHead.transform.localEulerAngles = new Vector3(0, headRotation.y, 0);
+    }
+}
