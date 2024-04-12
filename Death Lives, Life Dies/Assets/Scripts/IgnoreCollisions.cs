dd40c9850d8d2449c342ac77d2192bd2a0d6bf56
FarahKamleh <fkamleh8800@gmail.com>
2024-04-12 13:46:11 -0500

    Ignore Collisions
    The body for Death and his scythe no longer collide with each other.

---
+++ Death Lives, Life Dies/Assets/Scripts/IgnoreCollisions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollisions : MonoBehaviour
+{
+    // gameobjects whose colliders are of interest
+    public GameObject deathBody;
+    public GameObject scythe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ensure death body and scythe do not collide
+        Physics.IgnoreCollision(deathBody.GetComponent<Collider>(), scythe.GetComponent<Collider>());
+    }
+}
