ce81608dd8b14ca70df114851d2a6ec16c3ae820
julianmackenzie <jbm5@uic.edu>
2024-04-16 21:15:54 -0500

    Win Scene (probably) Implemented
    Will need to review in the CAVE

---
+++ Death Lives, Life Dies/Assets/WinConditions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinConditions : MonoBehaviour
+{
+
+    public GameObject Death;
+    public GameObject Newborn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Death.GetComponent<CharacterInfo>().health == 0) {
+            SceneManager.LoadScene("Win Scene Life");
+        }
+        if (Newborn.GetComponent<CharacterInfo>().health == 0) {
+            SceneManager.LoadScene("Win Scene Death");
+        }
+        
+    }
+}
